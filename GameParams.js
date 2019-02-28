77f7b8c8af6f58a14cbae3a0af6c63c62970ae17
NitheeshPrabu <nitheeshprabu.s@gmail.com>
2019-02-28 16:05:48 +0530

    Collecting Euclidean run results

--- GameParams.js
+++ GameParams.js
@@ -1,79 +1,79 @@
 class GameParams {
 
-    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	//constructor
-    constructor() {
-        this.tileSize = 50;
-        this.xoff = 80;
-        this.yoff = 100;
+	constructor() {
+		this.tileSize = 50;
+		this.xoff = 80;
+		this.yoff = 100;
 
-        //human playing vars
-        this.humanPlaying = false;
-        this.left = false;
-        this.right = false;
-        this.up = false;
-        this.down = false;
-        this.p;
+		//human playing vars
+		this.humanPlaying = false;
+		this.left = false;
+		this.right = false;
+		this.up = false;
+		this.down = false;
+		this.p;
 
-        //arrays
-        this.tiles = [];
-        this.shortGoals = [];
-        this.solids = [];
-        this.dots = [];
-        this.savedDots = [];
+		//arrays
+		this.tiles = [];
+		this.shortGoals = [];
+		this.solids = [];
+		this.dots = [];
+		this.savedDots = [];
 
-        this.showBest = false;
-        this.showNothing = true;
+		this.showBest = false;
+		this.showNothing = true;
 
-        this.winArea;
+		this.winArea;
 
-        //gen replay vars
-        this.replayGens = false;
-        this.genPlayer;
-        this.upToGenPos = 0;
+		//gen replay vars
+		this.replayGens = false;
+		this.genPlayer;
+		this.upToGenPos = 0;
 
-        //population vars
-        this.numberOfSteps = 10;
-        this.testPopulation;
+		//population vars
+		this.numberOfSteps = 10;
+		this.testPopulation;
 
-        this.winCounter = -1;
+		this.winCounter = -1;
 
-        //population size vars
-        this.populationSize = 500;
-        this.popPlus;
-        this.popMinus;
+		//population size vars
+		this.populationSize = 500;
+		this.popPlus;
+		this.popMinus;
 
-        //mutation rate vars
-        this.mutationRate = 0.01;
-        this.mrPlus;
-        this.mrMinus;
+		//mutation rate vars
+		this.mutationRate = 0.01;
+		this.mrPlus;
+		this.mrMinus;
 
-        //evolution speed vars
-        this.evolutionSpeed = 100;
-        this.speedPlus;
-        this.speedMinus;
+		//evolution speed vars
+		this.evolutionSpeed = 150;
+		this.speedPlus;
+		this.speedMinus;
 
-        this.increaseMovesBy = 5;
-        this.movesPlus;
-        this.movesMinus;
+		this.increaseMovesBy = 10;
+		this.movesPlus;
+		this.movesMinus;
 
-        this.increaseEvery = 5;
-        this.everyPlus;
-        this.everyMinus;
+		this.increaseEvery = 2;
+		this.everyPlus;
+		this.everyMinus;
 
-        this.nextConnectionNo = 1000;
+		this.nextConnectionNo = 1000;
 
-        this.firstClick = true;
-        this.showedCoins = false;
+		this.firstClick = true;
+		this.showedCoins = false;
 
-        this.pxm;
-        this.pym;
-        this.hasCoins;
+		this.pxm;
+		this.pym;
+		this.hasCoins;
 
-        this.distanceParam = 2; //1 - Manhatten, 2 - Euclidean, 3 - Minkowski
+		this.distanceParam = 2; //1 - Manhatten, 2 - Euclidean, 3 - Minkowski
 
-        this.playerThreshold = 1;
-        this.currentSimDetails = null;
-        this.simGenLimit = 100;
-    }
+		this.playerThreshold = 5;
+		this.currentSimDetails = null;
+		this.simGenLimit = 100;
+	}
 }
---
+++ run-results/calc.py
@@ -0,0 +1,73 @@
+import os
+import csv
+
+metrics = ['euclidean', 'manhatten']
+levels = [1, 2, 3, 4, 5]
+best_sizes = [1, 5, 25, 50]
+
+data = {0: ['converges', 'firstSteps', 'goalCount',
+            'finalGen', 'finalSteps', 'finalGoalCount']}
+results = {0: ['average_converges', 'average_first_step', 'average_goal_count', 'average_final_steps',
+               'average_final_goal_count', 'null_percent', 'percent_increase', 'best_size', 'level', 'metric']}
+
+for metric in metrics:
+    data[metric] = {}
+    results[metric] = {}
+    print("Starting for metric " + str(metric) + "...\n")
+    for level in levels:
+        data[metric][level] = {}
+        results[metric][level] = {}
+        print("Starting for level " + str(level) + "...\n")
+        for best_size in best_sizes:
+            data[metric][level][best_size] = []
+            results[metric][level][best_size] = []
+            with open(os.path.dirname(os.path.abspath(__file__))+'/'+str(metric)+'/level'+str(level)+'-'+str(best_size)+'.csv', 'r') as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                count = 0
+                for row in csv_reader:
+                    if count != 0:
+                        row = [int(val) if val else -1 for val in row]
+                        data[metric][level][best_size].append(row)
+                    count += 1
+                data[metric][level][best_size] = data[metric][level][best_size][1:]
+
+                # find percent null, average, percent improvement in steps, percent improvement in goal count
+                total_count = len(data[metric][level][best_size])
+                total_null_count = 0
+                running_sums = [0, 0, 0, 0, 0, 0]
+                for row in data[metric][level][best_size]:
+                    if row[0] == -1:
+                        total_null_count += 1
+                        continue
+                    else:
+                        for i in range(0, 6):
+                            running_sums[i] += row[i]
+                averages = [val/total_count for val in running_sums]
+                # print(averages)
+                averages = averages[0:3] + averages[4:]
+                # print(averages)
+                null_percent = round(
+                    float(total_null_count * 100.0 / total_count), 2)
+                percent_increase_steps = round(
+                    float((averages[1]-averages[3]) * 100.0 / averages[1]), 2)
+                percent_increase_goal_count = round(
+                    float((averages[4]-averages[2]) * 100.0 / averages[2]), 2)
+                percent_increase = round(float(
+                    percent_increase_steps * percent_increase_goal_count * 10000.0 / (500*120)), 2)
+                results[metric][level][best_size] = averages + \
+                    [null_percent, percent_increase, best_size, level, metric]
+            csv_file.close()
+            print("Best Size " + str(best_size) + " done.")
+        print("Level " + str(level) + " done.\n")
+    print("Metric " + str(metric) + " done.\n")
+
+with open(os.path.dirname(os.path.abspath(__file__))+'/results.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(results[0])
+    for metric in metrics:
+        for level in levels:
+            for best_size in best_sizes:
+                writer.writerow(results[metric][level][best_size])
+csv_file.close()
+
+print("File write complete.")
